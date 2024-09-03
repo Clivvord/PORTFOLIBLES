d9cd96853ea59296172a603bf6d4f69d3343153e
Clivvordd@gmail.com <you@example.com>
2024-09-04 00:12:52 +0100

    web dev design

--- auth.ts
+++ auth.ts
@@ -1,13 +1,25 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import GoogleProvider from "next-auth/providers/google";
+import TwitterProvider from "next-auth/providers/twitter";
+import FacebookProvider from "next-auth/providers/facebook";
+import {Auth} from "@auth/core"
  
 export const { handlers,auth,signIn,signOut } = NextAuth({
   ...authConfig,
   providers: [
     GoogleProvider({
         clientId:process.env.GOOGLE_CLIENT_ID,
-        clientSecret:process.env.GOOGLE_CLIENT_SECRECT
+        clientSecret:process.env.GOOGLE_CLIENT_SECRECT,
     }),
-  ],
+    TwitterProvider({
+        clientId:process.env.TWITTER_CLIENT_ID,
+        clientSecret:process.env.TWITTER_CLIENT_SECRET,
+    }),
+    
+    FacebookProvider({
+        clientId: process.env.FACEBOOK_APP_ID,
+        clientSecret:process.env.FACEBOOK_APP_SECRET,
+    }),
+],
 });
