b80244dccbee8e0e994a3702996c1ebc6f5b73d0
Clivvordd@gmail.com <you@example.com>
2024-09-04 12:10:07 +0100

    authorisation

--- middleware.ts
+++ middleware.ts
@@ -6,3 +6,9 @@ export default NextAuth(authConfig).auth;
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
+const toRegex = require('path-to-regexp');
+
+const pathTemplaten= '/users/:usersId';
+const regex = toRegex(pathTemplate);
+
+console.log(regex.exec('/users/123')); // [ '/users/123', '123' ]
